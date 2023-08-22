61840090cf970e17e29bc8db465591d1bad6025a
kfollesdal <kristoffer.follesdal@eviny.no>
2023-08-22 17:23:39 +0200

    Add merg function, moved from yggdrasil

--- flake.nix
+++ flake.nix
@@ -8,11 +8,14 @@
       inherit midgardOverlay mapMidgardOverlay;
 
       overlays.libMidgard = midgardOverlay (final: prev: let
-        inherit (prev.lib) mapAttrs' nameValuePair removeSuffix;
+        inherit (prev.lib) mapAttrs' nameValuePair removeSuffix foldAttrs;
       in {
         lib = {
           importDir = dir: mapAttrs' (n: v: nameValuePair (removeSuffix ".nix" n) (import "${dir}/${n}")) (readDir dir);
           systemdExpoRestart = import ./systemd prev;
+
+          # Merge list of attributes into a single attribute set
+          merge = foldAttrs (x: y: x // y) { };
         };
       });
   };
